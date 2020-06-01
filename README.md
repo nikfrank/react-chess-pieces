614f5bc12fccfeb17182e1f88d6ca227a7f2419b
nik frank <nikbfrank@gmail.com>
2020-06-01 22:08:27 +0300

    fix complaints about windows case sensitivity aka not

--- README.md
+++ README.md
@@ -66,7 +66,7 @@ export default App;
 
 the Piece Component renders into an unstyled svg, so you should be able to style it however you please
 
-all svg are available by direct import as well (but they are named case sensitively, sorry not sorry to windows people)
+all svg are available by direct import as well via /dist/svg-index
 
 this example is missing all the CSS - if you want to learn how to build a chessboard in react checkout my [workshop here](https://github.com/nikfrank/chess-workshop)
 
--- src/svg-index.js
+++ src/svg-index.js
@@ -1,15 +1,15 @@
-import K from './K.svg';
-import k from './k.svg';
-import Q from './Q.svg';
-import q from './q.svg';
-import R from './R.svg';
-import r from './r.svg';
-import N from './N.svg';
-import n from './n.svg';
-import B from './B.svg';
-import b from './b.svg';
-import P from './P.svg';
-import p from './p.svg';
+import K from './K-white.svg';
+import k from './k-black.svg';
+import Q from './Q-white.svg';
+import q from './q-black.svg';
+import R from './R-white.svg';
+import r from './r-black.svg';
+import N from './N-white.svg';
+import n from './n-black.svg';
+import B from './B-white.svg';
+import b from './b-black.svg';
+import P from './P-white.svg';
+import p from './p-black.svg';
 
 export default {
   K, k, Q, q, R, r, N, n, B, b, P, p,
