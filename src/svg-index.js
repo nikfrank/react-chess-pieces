e1d56f446df675e6f7ad3b655ad47afcf6549651
nik frank <nikbfrank@gmail.com>
2020-06-01 22:01:33 +0300

    104 svg index

---
+++ src/svg-index.js
@@ -0,0 +1,16 @@
+import K from './K.svg';
+import k from './k.svg';
+import Q from './Q.svg';
+import q from './q.svg';
+import R from './R.svg';
+import r from './r.svg';
+import N from './N.svg';
+import n from './n.svg';
+import B from './B.svg';
+import b from './b.svg';
+import P from './P.svg';
+import p from './p.svg';
+
+export default {
+  K, k, Q, q, R, r, N, n, B, b, P, p,
+};
